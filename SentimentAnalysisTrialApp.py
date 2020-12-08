06658e345155f1d7526f3ef352726a9325c2c025
Omkar Sawant <omkarsawant5029@gmail.com>
2020-12-08 12:47:12 +0530

    Create trial flask app for sentiment analysis

---
+++ SentimentAnalysisTrialApp.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+import re
+from flask import Flask
+from flask import jsonify
+from SentimentAnalysisLoadData import load_tokenizer, load_model, get_sentiment_prediction
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Welcome to Pi-Blog'
+
+@app.route('/sentiment/<text>')
+def wordsentiment(text):
+
+    """
+        Returns sentiment of given text
+    """
+
+    wordString = text
+    model = load_model()
+    tokenizer = load_tokenizer()
+    sentiment, score = get_sentiment_prediction(model, tokenizer, wordString)
+    return sentiment
+
+if __name__ == "__main__":
+    app.run(debug=True)
